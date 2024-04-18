f41c8b8506a187d82010a1b5aff1945866362892
HeronErin <HeronErin@protonmail.com>
2024-04-18 14:22:35 -0400

    Keyword consumer utils

--- source/parsing/treegen/expressionParser.d
+++ source/parsing/treegen/expressionParser.d
@@ -3,25 +3,45 @@ module parsing.treegen.expressionParser;
 import parsing.treegen.astTypes;
 import parsing.tokenizer.tokens;
 import parsing.tokenizer.make_tokens;
+import tern.typecons.common : Nullable;
 import errors;
 
-AstNode parseExpression(Token[] tokens)
-{
-    AstNode[] nodes = parenthesisExtract(tokens);
-    nodes.writeln;
-    
-    AstNode a;
-    a.action = AstAction.Scope;
-    return a;
+
+// Line types;
+///// 1. Variable declairation. 
+/////     NameUnit followed by NameUnit
+
+enum LineType{
+    Declaration,
+    Expression
 }
 
+
 import std.stdio;
+void parseLine(Token[] tokens, ScopeParsingMode mode)
+{
+    size_t index = 0;
+    Nullable!Token firstToken = tokens.nextNonWhiteToken(index);
+    if (firstToken.ptr == null)
+        throw new SyntaxError("Expected a statement");
+    // Nullable!LineType;
+    // Determine line type
+    if (mode.allowVariableDefinitions){
+        
+    }
+
+
+}
+
 
 unittest
 {
-    // AstNode[] asts = parenthesisExtract(tokenizeText("int x = 2\nint b = x++ + 4; int c = x++ + b++")); // 4, 7, 8
-    parseExpression(tokenizeText("int x = abs(12)+(8*pop.open()();")); // 4, 7, 8
-
-    // asts.writeln;
-    // asts[1].expressionComponents.writeln;
+    parseLine(tokenizeText("int x = 4;"), ScopeParsingMode(
+        false,
+        false,
+        true,
+        true,
+        false,
+        false
+    ));
 }
---
+++ source/parsing/treegen/keywords.d
@@ -0,0 +1,83 @@
+module parsing.treegen.keywords;
+import parsing.tokenizer.tokens;
+import std.string : indexOf;
+import errors;
+
+const dchar[][] FUNC_STYLE_KEYWORD = [
+    "align".makeUnicodeString
+];
+
+const dchar[][] KEYWORDS = [
+    "delete".makeUnicodeString,
+    "import".makeUnicodeString,
+    "alias".makeUnicodeString,
+    "module".makeUnicodeString,
+    "pure".makeUnicodeString,
+    "public".makeUnicodeString,
+    "private".makeUnicodeString,
+    "static".makeUnicodeString,
+    "abstract".makeUnicodeString,
+    "partial".makeUnicodeString,
+    "unsafe".makeUnicodeString,
+    "@tapped".makeUnicodeString,
+    "inline".makeUnicodeString,
+    "const".makeUnicodeString,
+    "mustuse".makeUnicodeString,
+    "mixin".makeUnicodeString,
+] ~ FUNC_STYLE_KEYWORD;
+
+
+private bool scontains(const(dchar[][]) list, const(dchar[]) str){
+    foreach (const(dchar[]) list_item; list)
+    {
+        if (list_item == str)
+            return true;
+    }
+    return false;
+}
+
+import std.stdio;
+// Keywords break many other parts of parsing, so best to get them out of the way first
+dchar[][] skipAndExtractKeywords(ref Token[] tokens, ref size_t index){
+    dchar[][] keywords;
+    while(index < tokens.length && tokens[index].tokenVariety == TokenType.Letter){
+        dchar[] data = tokens[index].value;
+        
+        bool isKeyword = KEYWORDS.scontains(data);
+        if (!isKeyword) break;
+        bool isFucKeyword = FUNC_STYLE_KEYWORD.scontains(data);
+        if (!isFucKeyword)
+            keywords ~= data;
+        else{
+            
+            if(index+1 >= tokens.length || tokens[index+1].tokenVariety != TokenType.OpenBraces)
+                throw new SyntaxError("Keyword must have arguments");
+
+            dchar[] keyword = new dchar[data.length];
+            keyword[0..$] = data;
+            while (++index < tokens.length && tokens[index].tokenVariety != TokenType.CloseBraces){
+                keyword ~= tokens[index].value;
+            }
+            if (index >= tokens.length)
+                throw new SyntaxError("Keyword apears to have open parenthesis");
+            keyword ~= tokens[index].value;
+            keywords ~= keyword;
+        }
+        tokens.nextNonWhiteToken(index);
+        index++;
+    }
+    return keywords;
+}
+
+
+unittest
+{
+    import parsing.tokenizer.make_tokens;
+    Token[] tokens = tokenizeText("align(an invalid alignment) abstract pure int x();");
+    
+    size_t index = 0;
+    assert(
+        ["align(an invalid alignment)".makeUnicodeString, "abstract".makeUnicodeString, "pure".makeUnicodeString]
+          == skipAndExtractKeywords(tokens, index));
+    assert(tokens[index].value == "int".makeUnicodeString );
+}
